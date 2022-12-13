3e99af9866ea96e59ad57bb2eea7fedf4cd00c73
MargaretHopkins <63251494+MargaretHopkins@users.noreply.github.com>
2022-12-13 15:48:42 -0500

    Update run_simulations.m

--- 618ProjectHopkins/run_simulations.m
+++ 618ProjectHopkins/run_simulations.m
@@ -1,5 +1,8 @@
+%for testing multiple values of Eeqy for a given flip and pmax
+flip = 350;
+pmax = 3000;
 for g=-0.1E-3:0.3E-3:2.1E-3
-    [t, p] = main_Margaret(g);
+    [t, p] = main_Margaret(g,flip, pmax);
     figure()
     plot(t, p)
     title(g)
